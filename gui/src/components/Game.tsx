2b584e0d1f40824ed070ce9e27788ca16b73aac3
voikin <woysen@gmail.com>
2024-05-30 23:07:20 +0300

    gui: add game actions && add resizing && add army rows

--- gui/src/components/Game.tsx
+++ gui/src/components/Game.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSpring, animated } from "react-spring";
 import Spritesheet from "react-responsive-spritesheet";
-import { Army, ArmyType, UnitType } from "../types";
+import { Army, ArmyType, GameState, Strategy, UnitType } from "../types";
 import { spriteFactory } from "../factories";
-import { Tooltip } from "antd";
+import { Tooltip, Select, Button, Card } from "antd";
+import { useMutation } from "react-query";
+import { nextStep, undo, redo, changeStrategy } from "../api";
 
+const { Option } = Select;
 const AnimatedSpritesheet = animated(Spritesheet);
 
 const renderUnit = (
@@ -13,8 +16,6 @@ const renderUnit = (
   maxHeight: number,
   spacing?: number
 ) => {
-  console.log({ spacing });
-
   const { image, width, height, steps, fps, offsetX, offsetY } =
     spriteFactory.createSprite(armyType, unit);
   const springProps = useSpring({
@@ -23,6 +24,10 @@ const renderUnit = (
     reset: true,
     config: { duration: 500 },
   });
+  const isLittleUnit = armyType === 'horde' && ["light_swordsman", "heavy_swordsman", "paladin"].includes(unit)
+  // if (isLittleUnit) {
+  //   spacing = spacing && spacing + 16
+  // }
 
   return (
     <Tooltip title={unit} key={unit}>
@@ -62,68 +67,185 @@ const renderUnit = (
 };
 
 const renderArmy = (armyType: ArmyType, army: Army, containerWidth: number) => {
-  // находим максимальные высоту и ширину юнитов для корректного выравнивания
+  // находим максимальные высоту юнитов для корректного выравнивания
   const maxHeight = Math.max(
     ...army.flatMap((row) =>
       row.map((unit) => spriteFactory.createSprite(armyType, unit.type).height)
     )
   );
 
-  const unitWidths = army.flatMap((row) =>
-    row.map((unit) => spriteFactory.createSprite(armyType, unit.type).width)
-  );
+  return army.map((row, rowIndex) => {
+    // вычисляем общую ширину юнитов в строке и соответствующий spacing
+    const rowUnitWidths = row.map(
+      (unit) => spriteFactory.createSprite(armyType, unit.type).width
+    );
 
-  const totalUnitWidth = unitWidths.reduce((acc, width) => acc + width, 0);
-  let spacing = (containerWidth - totalUnitWidth) / (army[0].length - 1);
+    const totalRowUnitWidth = rowUnitWidths.reduce(
+      (acc, width) => acc + width,
+      0
+    );
+    let spacing = (containerWidth - totalRowUnitWidth) / (row.length - 1);
+    if (spacing > 0) {
+      spacing = 0;
+    }
 
-  console.log({ spacing });
-
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        flexDirection: armyType === "alliance" ? "row-reverse" : "row",
-        flexWrap: "nowrap",
-      }}
-    >
-      {army.flatMap((row) =>
-        row.map((unit, i) => {
-          console.log({unit, i});
+    return (
+      <div
+        key={`row-${rowIndex}`}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: armyType === "alliance" ? "row-reverse" : "row",
+          flexWrap: "nowrap",
+        }}
+      >
+        {row.map((unit, i) => {
           if (i === 0) {
-            return renderUnit(armyType, unit.type, maxHeight, 0);
+            return renderUnit(armyType, unit.type, maxHeight, -20);
           }
           return renderUnit(armyType, unit.type, maxHeight, spacing);
-        })
-      )}
-    </div>
-  );
+        })}
+      </div>
+    );
+  });
 };
 
 export type GameProps = {
   alliance: Army;
   horde: Army;
+  setAlliance: React.Dispatch<React.SetStateAction<Army>>;
+  setHorde: React.Dispatch<React.SetStateAction<Army>>;
 };
 
-export const Game: React.FC<GameProps> = ({ alliance, horde }) => {
-  const containerWidth = 1000; // ширина контейнера
-  console.log({alliance});
-  
+export const Game: React.FC<GameProps> = ({
+  alliance,
+  horde,
+  setAlliance,
+  setHorde,
+}) => {
+  const [width, setWidth] = useState(0);
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    const handleResize = (entries) => {
+      for (let entry of entries) {
+        setWidth(entry.contentRect.width);
+      }
+    };
+
+    const resizeObserver = new ResizeObserver(handleResize);
+
+    if (divRef.current) {
+      resizeObserver.observe(divRef.current);
+    }
+
+    return () => {
+      if (divRef.current) {
+        resizeObserver.unobserve(divRef.current);
+      }
+    };
+  }, []);
+
+  const [selectedStrategy, setSelectedStrategy] =
+    useState<Strategy>("one_line");
+
+  const setGameState = (gameState: GameState) => {
+    setAlliance(gameState.alliance);
+    setHorde(gameState.horde);
+  };
+
+  const nextStepMutation = useMutation(nextStep, {
+    onSuccess: ({ data: newGameState }) => {
+      setGameState(newGameState);
+    },
+  });
+
+  const undoMutation = useMutation(undo, {
+    onSuccess: ({ data: newGameState }) => {
+      setGameState(newGameState);
+    },
+  });
+
+  const redoMutation = useMutation(redo, {
+    onSuccess: ({ data: newGameState }) => {
+      setGameState(newGameState);
+    },
+  });
+
+  const changeStrategyMutation = useMutation(
+    () => changeStrategy(selectedStrategy),
+    {
+      onSuccess: ({ data: newGameState }) => {
+        setGameState(newGameState);
+      },
+    }
+  );
 
   return (
-    <div
-      style={{
-        width: containerWidth,
-        height: 600,
-        backgroundColor: "#10bb99",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: "20px",
-      }}
-    >
-      <div>{renderArmy("alliance", alliance, containerWidth / 2)}</div>
-      <div>{renderArmy("horde", horde, containerWidth / 2)}</div>
-    </div>
+    <Card>
+      <div ref={divRef}>
+        <div
+          style={{
+            minHeight: 600,
+            backgroundColor: "#758D45",
+            borderRadius: 20,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "20px",
+          }}
+        >
+          <div>{renderArmy("alliance", alliance, width / 2)}</div>
+          <div>{renderArmy("horde", horde, width / 2)}</div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: 8,
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "20px",
+            textAlign: "center",
+          }}
+        >
+          <Button
+            onClick={() => nextStepMutation.mutate()}
+            style={{ marginRight: "10px" }}
+          >
+            Следующий ход
+          </Button>
+          <Button
+            onClick={() => undoMutation.mutate()}
+            style={{ marginRight: "10px" }}
+          >
+            Отменить ход
+          </Button>
+          <Button
+            onClick={() => redoMutation.mutate()}
+            style={{ marginRight: "10px" }}
+          >
+            Повторить ход
+          </Button>
+          <div>
+            <Select
+              value={selectedStrategy}
+              onChange={(value: Strategy) => setSelectedStrategy(value)}
+              style={{ width: "200px", marginLeft: "10px" }}
+            >
+              <Option value="one_line">Одна линия</Option>
+              <Option value="multi_line">Много линий</Option>
+              <Option value="wall_to_wall">Стена на стену</Option>
+            </Select>
+            <Button
+              onClick={() => changeStrategyMutation.mutate()}
+              style={{ marginLeft: "10px" }}
+            >
+              Поменять стратегию
+            </Button>
+          </div>
+        </div>
+      </div>
+    </Card>
   );
 };
