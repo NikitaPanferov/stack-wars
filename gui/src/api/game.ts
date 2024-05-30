105d1afc49e7a172890057d9fa50ad2b0f5caee1
voikin <woysen@gmail.com>
2024-05-30 23:07:20 +0300

    gui: add game actions && add resizing && add army rows

--- gui/src/api/game.ts
+++ gui/src/api/game.ts
@@ -1,5 +1,5 @@
 import { AxiosResponse } from "axios";
-import { GameState } from "../types";
+import { GameState, Strategy } from "../types";
 import { api } from "./api";
 
 export async function startGame(
@@ -7,4 +7,22 @@ export async function startGame(
   horde: string[]
 ): Promise<AxiosResponse<GameState>> {
   return api.post<GameState>("/game/start", { alliance, horde });
+}
+
+export async function nextStep() {
+  return api.get<GameState>("/game/next_step");
+}
+
+export async function undo() {
+  return api.get<GameState>("/game/undo");
+}
+
+export async function redo() {
+  return api.get<GameState>("/game/redo");
+}
+
+export async function changeStrategy(strategy: Strategy) {
+  return api.get<GameState>("/game/change_strategy", {
+    params: { strategy },
+  });
 }
