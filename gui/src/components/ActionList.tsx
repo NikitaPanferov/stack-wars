05b0561fa45e2cd285f1bfdcc79d1cc4e2398e87
voikin <woysen@gmail.com>
2024-05-31 01:35:52 +0300

    gui: add handle of next step

---
+++ gui/src/components/ActionList.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Action, ActionType } from "../types";
+import { List } from "antd";
+
+type ActionListProps = {
+  actions: Action[];
+};
+
+const actionTypeToText = {
+  [ActionType.death]: "Погиб",
+  [ActionType.attack]: "Атаковал",
+  [ActionType.arrow]: "Выпустил стрелу",
+  [ActionType.clone]: "Клонировался",
+  [ActionType.heal]: "Исцелился",
+  [ActionType.move]: "Переместился",
+};
+
+export const ActionList: React.FC<ActionListProps> = ({ actions }) => {
+  console.log({actions});
+
+  return (
+    <List
+      size="small"
+      bordered
+      dataSource={actions}
+      renderItem={(action) => (
+        <List.Item>
+          {`Юнит ${action.subject} ${actionTypeToText[action.type]} юнит ${
+            action.object
+          } на ${action.value}`}
+        </List.Item>
+      )}
+    />
+  );
+};
