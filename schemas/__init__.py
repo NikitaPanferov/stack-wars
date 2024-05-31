2d3341a41b361a5ed978a8bf271b55c6115b8acd
NikitaPanferov <nikpanfer@gmail.com>
2024-06-01 01:42:21 +0300

    fix

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -2,4 +2,4 @@ from .init_armies import InitArmiesDTO
 from .action import Action, ActionType
 from .unitDTO import UnitDTO
 from .next_stepDTO import NextStepDTO
-from .game_state import GameState
+from .game_state import GameState, GameStateDTO
